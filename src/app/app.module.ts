6a5e75e4ade24e54331e3a60e61d0b422b0d8c0c
Fekry Personal <fekrysaharudins@gmail.com>
2024-01-12 23:15:39 +0800

    [Home-Page] Add new files and update existing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StarRatingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   development: true,
-  API_BASE_URL: 'http://localhost:7000'
+  API_BASE_URL: 'http://localhost:7000/api/v1/'
 };
